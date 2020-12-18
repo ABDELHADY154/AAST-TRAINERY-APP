6345dd082d77bcb0a9250344ab113aab7c8c5db1
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2020-12-19 01:04:20 +0200

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>ABDELHADY</Text>
+export default class App extends Component {
+  componentDidMount() {
+    Font.loadAsync({
+      "sf-font": require("./assets/fonts/AbhayaLibre-Bold.ttf"),
+    });
+  }
 
-      <StatusBar style="auto" />
-    </View>
-  );
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{ fontSize: 40, fontFamily: "sf-font" }}>
+          AAST TRAINERY
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
