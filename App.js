d737df347c96c4319ac7c527d7b963f09095d2a8
Basma Mostafa <57597830+basmaamostafa@users.noreply.github.com>
2021-02-16 01:49:54 +0200

    forget password screen

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import SplashScreen from "./src/Components/Splash/SplashScreen";
 import Home from "./src/Components/Home/HomeScreen";
-import LoginForm from "./src/Components/Auth/LoginForm";
+import LoginForm from "./src/Components/Auth/ForgetPass";
+import ForgetPass from "./src/Components/Auth/ForgetPass";
 import RegisterScreen from "./src/Components/Auth/RegisterForm";
 import * as Font from "expo-font";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -86,7 +87,7 @@ export default function App({ navigation }) {
       isLoading: true,
       isSignout: false,
       userToken: null,
-    },
+    }
   );
 
   React.useEffect(() => {
@@ -108,16 +109,17 @@ export default function App({ navigation }) {
 
   const authContext = React.useMemo(
     () => ({
-      signIn: async data => {
+      signIn: async (data) => {
         dispatch({ type: "SIGN_IN", token: "dummy-auth-token" });
       },
       signOut: () => dispatch({ type: "SIGN_OUT" }),
-      signUp: async data => {
+      signUp: async (data) => {
         dispatch({ type: "SIGN_IN", token: "dummy-auth-token" });
       },
     }),
-    [],
+    []
   );
+  return <ForgetPass />;
 
   return (
     <AuthContext.Provider value={authContext}>
