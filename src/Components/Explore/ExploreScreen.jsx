f1d9852b92dbd594485eef0627050ad54fca74b6
Basma Mostafa <57597830+basmaamostafa@users.noreply.github.com>
2021-02-17 00:23:48 +0200

    search icon

--- src/Components/Explore/ExploreScreen.jsx
+++ src/Components/Explore/ExploreScreen.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import { View, StyleSheet, SafeAreaView, ScrollView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { CompanyCard } from "../Cards/CompanyCard";
 import { AdvisorCard } from "../Cards/AdvisorCard";
@@ -7,23 +7,41 @@ import { PromotedCard } from "../Cards/PromotedCard";
 import { SessionCard } from "../Cards/SessionCard";
 import { AdsCard } from "../Cards/AdsCard";
 import { AdsCardImg } from "../Cards/AdsCardImg";
+import { Container, Content, Button, Text, Right } from "native-base";
+import { Feather } from "@expo/vector-icons";
 
 export default class ExploreScreen extends Component {
   render() {
     return (
-      <View>
-        <SafeAreaView>
-          <ScrollView>
-            <AdvisorCard />
+      <Container style={{ marginBottom: 20 }}>
+        <Content style={{ marginTop: 55 }}>
+          <SafeAreaView>
+            <ScrollView>
+              <Button transparent>
+                <Right>
+                  <Feather
+                    name="search"
+                    size={28}
+                    color="#1E4274"
+                    style={{
+                      marginRight: 20,
+                    }}
+                  />
+                </Right>
+              </Button>
 
-            <AdsCard />
-            <AdsCardImg />
-            <CompanyCard />
-            <PromotedCard />
-            {/* <SessionCard /> */}
-          </ScrollView>
-        </SafeAreaView>
-      </View>
+              <AdvisorCard />
+
+              <AdsCard />
+              <AdsCardImg />
+              <CompanyCard />
+              <PromotedCard />
+              {/* <SessionCard /> */}
+            </ScrollView>
+          </SafeAreaView>
+        </Content>
+        <StatusBar style="auto" />
+      </Container>
     );
   }
 }
