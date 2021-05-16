cd0f43d2679dbe4c8336b21d4911c6379e097a5f
Mohamed Abdelhady Elshamy <62076892+ABDELHADY154@users.noreply.github.com>
2021-05-16 14:47:03 +0200

    student notification

--- src/Components/Notification/Notification.jsx
+++ src/Components/Notification/Notification.jsx
@@ -5,58 +5,103 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Button } from "galio-framework";
 import { Feather } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
+import { axios } from "../../Config/Axios";
+import Spinner from "react-native-loading-spinner-overlay";
+import { RefreshControl } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
 export default class Notification extends Component {
+  state = {
+    notifications: [],
+    loading: true,
+    refresh: false,
+  };
+  async componentDidMount() {
+    await axios
+      .get("/A/student/notifications")
+      .then(res => {
+        this.setState({
+          loading: false,
+          notifications: res.data.response.data,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  onRefresh = async () => {
+    this.setState({
+      refresh: true,
+    });
+    await axios
+      .get("/A/student/notifications")
+      .then(res => {
+        this.setState({
+          loading: false,
+          notifications: res.data.response.data,
+          refresh: false,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    // this.setState
+  };
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView>
+        <Spinner
+          visible={this.state.loading}
+          cancelable={false}
+          size="large"
+          color="#1E4274"
+          animation="fade"
+          overlayColor="rgba(255, 255, 255, 0.8)"
+          textStyle={{ color: "#1E4274", textAlign: "center" }}
+        />
+        <ScrollView
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refresh}
+              onRefresh={this.onRefresh}
+              colors={["#1E4274"]}
+            />
+          }
+        >
           <View style={{ marginVertical: 10 }}>
-            {/* rejection notification */}
-            <View
-              style={{
-                width: "99%",
-                marginBottom: 15,
-                borderRadius: 3,
-                backgroundColor: "#fff",
-                borderLeftColor: "#F44336",
-                borderLeftWidth: 5,
-                borderLeftRadius: 5,
-                shadowColor: "#000",
-                shadowOffset: {
-                  width: 0,
-                  height: 4,
-                },
-                shadowOpacity: 0.3,
-                shadowRadius: 4.65,
+            {this.state.notifications.length !== 0 ? (
+              this.state.notifications.map(e => {
+                if (e.category == "rejected") {
+                  return (
+                    <RejectCard
+                      key={e.id}
+                      data={e}
+                      navigation={this.props.navigation}
+                    />
+                  );
+                } else if (e.category == "success") {
+                  return (
+                    <AcceptedCard
+                      key={e.id}
+                      data={e}
+                      navigation={this.props.navigation}
+                    />
+                  );
+                } else if (e.category == "important") {
+                  return (
+                    <ImportantCard
+                      key={e.id}
+                      data={e}
+                      navigation={this.props.navigation}
+                    />
+                  );
+                }
+              })
+            ) : (
+              <Text></Text>
+            )}
 
-                elevation: 8,
-              }}
-            >
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  paddingVertical: 10,
-                }}
-              >
-                <Feather
-                  name="x-octagon"
-                  size={24}
-                  color="#F44336"
-                  style={{ marginHorizontal: 10 }}
-                />
-                <View style={{ width: "80%" }}>
-                  <Text style={{ color: "#1E4274", fontSize: 16 }}>
-                    Unfortunately, you were rejected in the applied internship
-                  </Text>
-                  <Text style={{ color: "#B1B0B0" }}>
-                    Tap for more information{" "}
-                  </Text>
-                </View>
-              </View>
-            </View>
-            {/* session review notification */}
-            <View
+            {/* <View
               style={{
                 width: "99%",
                 // height: 50,
@@ -100,7 +145,6 @@ export default class Notification extends Component {
                 </View>
               </View>
             </View>
-            {/* recommended notification */}
             <View
               style={{
                 width: "99%",
@@ -145,96 +189,9 @@ export default class Notification extends Component {
                 </View>
               </View>
             </View>
-            {/* accepted notification */}
-            <View
-              style={{
-                width: "99%",
-                // height: 50,
-                marginBottom: 15,
-                borderRadius: 3,
-                backgroundColor: "#fff",
-                borderLeftColor: "#4CAF50",
-                borderLeftWidth: 5,
-                borderLeftRadius: 5,
-                shadowColor: "#000",
-                shadowOffset: {
-                  width: 0,
-                  height: 4,
-                },
-                shadowOpacity: 0.3,
-                shadowRadius: 4.65,
+             */}
 
-                elevation: 8,
-              }}
-            >
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  paddingVertical: 10,
-                }}
-              >
-                <AntDesign
-                  name="checkcircleo"
-                  size={24}
-                  color="#4CAF50"
-                  style={{ marginHorizontal: 10 }}
-                />
-                <View style={{ width: "80%" }}>
-                  <Text style={{ color: "#1E4274", fontSize: 16 }}>
-                    Congratulations you got accepted in the applied internship{" "}
-                  </Text>
-                  <Text style={{ color: "#B1B0B0" }}>
-                    Tap for more information
-                  </Text>
-                </View>
-              </View>
-            </View>
             {/* booked session notification */}
-            <View
-              style={{
-                width: "99%",
-                // height: 50,
-                marginBottom: 15,
-                borderRadius: 3,
-                backgroundColor: "#fff",
-                borderLeftColor: "#4CAF50",
-                borderLeftWidth: 5,
-                borderLeftRadius: 5,
-                shadowColor: "#000",
-                shadowOffset: {
-                  width: 0,
-                  height: 4,
-                },
-                shadowOpacity: 0.3,
-                shadowRadius: 4.65,
-
-                elevation: 8,
-              }}
-            >
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  paddingVertical: 10,
-                }}
-              >
-                <AntDesign
-                  name="checkcircleo"
-                  size={24}
-                  color="#4CAF50"
-                  style={{ marginHorizontal: 10 }}
-                />
-                <View style={{ width: "80%" }}>
-                  <Text style={{ color: "#1E4274", fontSize: 16 }}>
-                    Your session has been booked successfully{" "}
-                  </Text>
-                  <Text style={{ color: "#B1B0B0" }}>
-                    Tap for more information
-                  </Text>
-                </View>
-              </View>
-            </View>
           </View>
         </ScrollView>
       </View>
@@ -250,3 +207,210 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
 });
+
+class RejectCard extends Component {
+  render() {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.999999999}
+        underlayColor="#DDDDDD"
+        onPress={() =>
+          this.props.data.type == "internship"
+            ? this.props.navigation.push("OpportunityPost", {
+                id: this.props.data.internship.id,
+              })
+            : this.props.navigation.push("Advising", {
+                id: this.props.data.session.id,
+              })
+        }
+      >
+        <View
+          style={{
+            width: "99%",
+            marginBottom: 15,
+            borderRadius: 3,
+            backgroundColor: "#fff",
+            borderLeftColor: "#F44336",
+            borderLeftWidth: 5,
+            borderLeftRadius: 5,
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 4,
+            },
+            shadowOpacity: 0.3,
+            shadowRadius: 4.65,
+
+            elevation: 8,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              paddingVertical: 10,
+            }}
+          >
+            <Feather
+              name="x-octagon"
+              size={24}
+              color="#F44336"
+              style={{ marginHorizontal: 10 }}
+            />
+            <View style={{ width: "80%" }}>
+              <Text style={{ color: "#1E4274", fontSize: 16 }}>
+                {this.props.data.type == "internship"
+                  ? this.props.data.internship.title
+                  : this.props.data.session.title}{" "}
+              </Text>
+              <Text style={{ color: "#1E4274", fontSize: 13 }}>
+                {this.props.data.message}{" "}
+              </Text>
+              <Text style={{ color: "#B1B0B0" }}>
+                Tap for more information{" "}
+              </Text>
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+class AcceptedCard extends Component {
+  render() {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.999999999}
+        underlayColor="#DDDDDD"
+        onPress={() =>
+          this.props.data.type == "internship"
+            ? this.props.navigation.push("OpportunityPost", {
+                id: this.props.data.internship.id,
+              })
+            : this.props.navigation.push("Advising", {
+                id: this.props.data.session.id,
+              })
+        }
+      >
+        <View
+          style={{
+            width: "99%",
+            // height: 50,
+            marginBottom: 15,
+            borderRadius: 3,
+            backgroundColor: "#fff",
+            borderLeftColor: "#4CAF50",
+            borderLeftWidth: 5,
+            borderLeftRadius: 5,
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 4,
+            },
+            shadowOpacity: 0.3,
+            shadowRadius: 4.65,
+
+            elevation: 8,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              paddingVertical: 10,
+            }}
+          >
+            <AntDesign
+              name="checkcircleo"
+              size={24}
+              color="#4CAF50"
+              style={{ marginHorizontal: 10 }}
+            />
+            <View style={{ width: "80%" }}>
+              <Text style={{ color: "#1E4274", fontSize: 16 }}>
+                {this.props.data.type == "internship"
+                  ? this.props.data.internship.title
+                  : this.props.data.session.title}{" "}
+              </Text>
+              <Text style={{ color: "#1E4274", fontSize: 13 }}>
+                {this.props.data.message}{" "}
+              </Text>
+              <Text style={{ color: "#B1B0B0" }}>Tap for more information</Text>
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+class ImportantCard extends Component {
+  render() {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.999999999}
+        underlayColor="#DDDDDD"
+        onPress={() =>
+          this.props.data.type == "internship"
+            ? this.props.navigation.push("OpportunityPost", {
+                id: this.props.data.internship.id,
+              })
+            : this.props.navigation.push("Advising", {
+                id: this.props.data.session.id,
+              })
+        }
+      >
+        <View
+          style={{
+            width: "99%",
+            // height: 50,
+            marginBottom: 15,
+            borderRadius: 3,
+            backgroundColor: "#fff",
+            borderLeftColor: "#007BC2",
+            borderLeftWidth: 5,
+            borderLeftRadius: 5,
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 4,
+            },
+            shadowOpacity: 0.3,
+            shadowRadius: 4.65,
+
+            elevation: 8,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              paddingVertical: 10,
+            }}
+          >
+            <Feather
+              name="alert-circle"
+              size={24}
+              color="#007BC2"
+              style={{ marginHorizontal: 10 }}
+            />
+            <View style={{ width: "80%" }}>
+              <Text style={{ color: "#1E4274", fontSize: 16 }}>
+                {this.props.data.type == "internship"
+                  ? this.props.data.internship.title
+                  : this.props.data.session.title}{" "}
+              </Text>
+              <Text style={{ color: "#1E4274", fontSize: 13 }}>
+                {this.props.data.message}{" "}
+              </Text>
+              <Text style={{ color: "#B1B0B0" }}>
+                Tap for more information{" "}
+              </Text>
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
